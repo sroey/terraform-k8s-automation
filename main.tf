9b1f2bc554fcd20c4db36d3e9c06db5710326aa4
sroey <sbz24k@gmail.com>
2026-02-18 10:04:15 +0000

    Changed to NodePort and used a variable for a welcome message.

--- main.tf
+++ main.tf
@@ -1,12 +1,16 @@
 module "primary_app" {
-  source        = "./modules/web_app"
-  app_name      = "webapp-alpha"
-  replica_count = 3  # This is your "N" value
+  source          = "./modules/web_app"
+  app_name        = "webapp-alpha"
+  replica_count   = 3 # This is your "N" value
+  node_port       = 30001
+  display_message = "Welcome to Alpha Stack!"
 }
 
 # BONUS: Implementing an additional stack with minimal code
 module "bonus_app" {
-  source        = "./modules/web_app"
-  app_name      = "webapp-bonus"
-  replica_count = 2
+  source          = "./modules/web_app"
+  app_name        = "webapp-bonus"
+  replica_count   = 2
+  node_port       = 30002
+  display_message = "This is the Bonus Server speaking:"
 }
--- modules/web_app/main.tf
+++ modules/web_app/main.tf
@@ -1,9 +1,16 @@
 variable "app_name" { type = string }
 variable "replica_count" { type = number }
 
+variable "display_message" {
+  type    = string
+  default = "Updated. Hello! You are being served by"
+}
+
+variable "node_port" { type = number }
+
 resource "kubernetes_deployment" "web_app" {
   metadata {
-    name = var.app_name
+    name   = var.app_name
     labels = { app = var.app_name }
   }
 
@@ -16,46 +23,46 @@ resource "kubernetes_deployment" "web_app" {
       metadata {
         labels = { app = var.app_name }
       }
-spec {
-      container {
-        image = "hashicorp/http-echo:latest"
-        name  = "web-server"
-        
-        # We pass the env variables into the text argument
-        # Note: http-echo will render $(VAR) if passed this way
-        args = ["-text=Hello! You are being served by Pod: $(MY_POD_NAME) | IP: $(MY_POD_IP)"]
+      spec {
+        container {
+          image = "hashicorp/http-echo:latest"
+          name  = "web-server"
 
-        env {
-          name = "MY_POD_NAME"
-          value_from {
-            field_ref {
-              field_path = "metadata.name"
+          # We pass the env variables into the text argument
+          # Note: http-echo will render $(VAR) if passed this way
+          args = ["-text=${var.display_message} Pod: $(MY_POD_NAME) | IP: $(MY_POD_IP)"]
+
+          env {
+            name = "MY_POD_NAME"
+            value_from {
+              field_ref {
+                field_path = "metadata.name"
+              }
             }
           }
-        }
-        env {
-          name = "MY_POD_IP"
-          value_from {
-            field_ref {
-              field_path = "status.podIP"
+          env {
+            name = "MY_POD_IP"
+            value_from {
+              field_ref {
+                field_path = "status.podIP"
+              }
             }
           }
-        }
 
-        port {
-          container_port = 5678
-        }
-
-        readiness_probe {
-          http_get {
-            path = "/"
-            port = 5678
+          port {
+            container_port = 5678
           }
-          initial_delay_seconds = 3
-          period_seconds        = 5
+
+          readiness_probe {
+            http_get {
+              path = "/"
+              port = 5678
+            }
+            initial_delay_seconds = 3
+            period_seconds        = 5
+          }
         }
       }
-    }
     }
   }
 }
@@ -69,6 +76,6 @@ resource "kubernetes_service" "web_service" {
       port        = 80
       target_port = 5678
     }
-    type = "LoadBalancer"
+    type = "NodePort"
   }
 }
