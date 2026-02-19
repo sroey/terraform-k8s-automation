d70c6be8a5291415f767f61732009907b434eae8
sroey <sbz24k@gmail.com>
2026-02-19 12:41:42 +0000

    Revert "Changed variables config for tests"
    This reverts commit 7f3b5c0a1cb1efe3c35fa1ece3f5b3a97fa8cbcc.

--- variables.tf
+++ variables.tf
@@ -1,24 +1,25 @@
 # Root variables.tf
 
-variable "app_config" {
-  description = "Shared configuration for web applications."
-  type = object({
+variable "app_configs" {
+  description = "Configurations for web applications."
+  type = map(object({
+    name            = string
     replica_count   = number
     node_port       = number
     display_message = string
-  })
+  }))
   default = {
-    replica_count   = 3
-    node_port       = 30001
-    display_message = "Welcome to Alpha Stack!"
-  }
-}
-
-variable "app_names" {
-  description = "Names for the web applications."
-  type = map(string)
-  default = {
-    primary = "webapp-alpha"
-    bonus   = "webapp-bonus"
+    primary = {
+      name            = "webapp-alpha"
+      replica_count   = 3
+      node_port       = 30001
+      display_message = "Welcome to Alpha Stack!"
+    }
+    bonus = {
+      name            = "webapp-bonus"
+      replica_count   = 2
+      node_port       = 30002
+      display_message = "This is the Bonus Server speaking:"
+    }
   }
 }
