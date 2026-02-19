7f3b5c0a1cb1efe3c35fa1ece3f5b3a97fa8cbcc
sroey <sbz24k@gmail.com>
2026-02-19 12:06:19 +0000

    Changed variables config for tests

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 module "primary_app" {
   source          = "./modules/web_app"
-  app_name        = var.app_configs.primary.name
-  replica_count   = var.app_configs.primary.replica_count
-  node_port       = var.app_configs.primary.node_port
-  display_message = var.app_configs.primary.display_message
+  app_name        = var.app_names.primary
+  replica_count   = var.app_config.replica_count
+  node_port       = var.app_config.node_port
+  display_message = var.app_config.display_message
 }
 
 
 # BONUS: Implementing an additional stack with minimal code
 module "bonus_app" {
   source          = "./modules/web_app"
-  app_name        = var.app_configs.bonus.name
-  replica_count   = var.app_configs.bonus.replica_count
-  node_port       = var.app_configs.bonus.node_port
-  display_message = var.app_configs.bonus.display_message
+  app_name        = var.app_names.bonus
+  replica_count   = var.app_config.replica_count
+  node_port       = var.app_config.node_port
+  display_message = var.app_config.display_message
 }
--- variables.tf
+++ variables.tf
@@ -1,25 +1,24 @@
 # Root variables.tf
 
-variable "app_configs" {
-  description = "Configurations for web applications."
-  type = map(object({
-    name            = string
+variable "app_config" {
+  description = "Shared configuration for web applications."
+  type = object({
     replica_count   = number
     node_port       = number
     display_message = string
-  }))
+  })
   default = {
-    primary = {
-      name            = "webapp-alpha"
-      replica_count   = 3
-      node_port       = 30001
-      display_message = "Welcome to Alpha Stack!"
-    }
-    bonus = {
-      name            = "webapp-bonus"
-      replica_count   = 2
-      node_port       = 30002
-      display_message = "This is the Bonus Server speaking:"
-    }
+    replica_count   = 3
+    node_port       = 30001
+    display_message = "Welcome to Alpha Stack!"
+  }
+}
+
+variable "app_names" {
+  description = "Names for the web applications."
+  type = map(string)
+  default = {
+    primary = "webapp-alpha"
+    bonus   = "webapp-bonus"
   }
 }
