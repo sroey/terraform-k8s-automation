b8ef0ff672863953ba530d2b6e3bd1d54b667a1b
sroey <sbz24k@gmail.com>
2026-02-21 12:07:01 +0000

    Create modules with for each

--- main.tf
+++ main.tf
@@ -1,17 +1,8 @@
-module "primary_app" {
-  source          = "./modules/web_app"
-  app_name        = var.app_configs.primary.name
-  replica_count   = var.app_configs.primary.replica_count
-  display_message = var.app_configs.primary.display_message
-  ingress_host    = "primary.web-app.local"
-}
+module "web_app" {
+  for_each = var.app_configs
 
-
-# BONUS: Implementing an additional stack with minimal code
-module "bonus_app" {
   source          = "./modules/web_app"
-  app_name        = var.app_configs.bonus.name
-  replica_count   = var.app_configs.bonus.replica_count
-  display_message = var.app_configs.bonus.display_message
-  ingress_host    = "bonus.web-app.local"
+  app_name        = each.value.name
+  replica_count   = each.value.replica_count
+  display_message = each.value.display_message
 }
--- outputs.tf
+++ outputs.tf
@@ -1,18 +1,7 @@
-output "primary_app_message" {
-  value       = module.primary_app.display_message
-  description = "The display message for the primary (alpha) app."
-}
-
-output "bonus_app_message" {
-  value       = module.bonus_app.display_message
-  description = "The display message for the bonus app."
-}
-
 
 output "web_app_urls" {
   description = "URLs to access web applications"
   value = {
-    primary = "curl -H 'Host: primary.web-app.local' http://localhost:8890/"
-    bonus   = "curl -H 'Host: bonus.web-app.local' http://localhost:8890/"
+    for key, value in var.app_configs : key => "curl http://localhost:8080/${value.name}"
   }
 }
