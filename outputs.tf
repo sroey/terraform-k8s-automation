d4a0ddd50195305ce01fa278155fe6f188a19657
sroey <sbz24k@gmail.com>
2026-02-23 17:11:34 +0000

    adding a comment for outputs

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,11 @@ output "web_app_urls" {
   }
 }
 
+
+# This output pulls display messages from module outputs for demonstration purposes.
+# These can also be accessed directly from var.app_configs from .tfvars:
+# value = { for key, value in var.app_configs : key => value.display_message }
+  
 output "display_messages" {
   description = "The display messages passed to each web app module."
   value       = { for key, mod in module.web_app : key => mod.display_message }
